968a1b6ee63b8d0c4477fb3062f64ba1e8b53a2a
Leandro Ramos <Leandro.Ramos@intellisite.io>
2023-08-31 01:00:13 -0300

    [master] implemented min number of steps

--- src/cli/water_jug_problem/problem_solver.py
+++ src/cli/water_jug_problem/problem_solver.py
@@ -29,6 +29,38 @@ def _water_jug_problem_has_solution(
     )
 
 
+def water_jug_solver_from(jug_capacity_1, jug_capacity_2, target, start_jug):
+    steps = []
+    jugs = [0, 0]
+    capacities = [jug_capacity_1, jug_capacity_2]
+    steps = []
+    other_jug = (start_jug + 1) % 2
+
+    while jugs[0] != target and jugs[1] != target:
+        if jugs[start_jug] == 0:
+            jugs[start_jug] = capacities[start_jug]
+            steps.append(
+                f"Fill jug {start_jug+1} ({capacities[start_jug]} Gallons)"
+            )
+        elif jugs[other_jug] == capacities[other_jug]:
+            jugs[other_jug] = 0
+            steps.append(
+                f"Empty jug {other_jug+1} ({capacities[other_jug]} "
+                f"Gallons)"
+            )
+        else:
+            transfer = min(
+                jugs[start_jug], capacities[other_jug] - jugs[other_jug]
+            )
+            jugs[start_jug] -= transfer
+            jugs[other_jug] += transfer
+            steps.append(
+                f"Pour {transfer} Gallons from jug {start_jug+1} to jug "
+                f"{other_jug+1}"
+            )
+    return steps
+
+
 def water_jug_solver(jug_capacity_1, jug_capacity_2, target):
     steps = None
 
@@ -37,21 +69,21 @@ def water_jug_solver(jug_capacity_1, jug_capacity_2, target):
         jug_capacity_2=jug_capacity_2,
         target=target,
     ):
-        jug1 = 0
-        jug2 = 0
-        steps = []
-
-        while jug1 != target and jug2 != target:
-            if jug1 == 0:
-                jug1 = jug_capacity_1
-                steps.append(f"Fill jug 1 ({jug_capacity_1} Gallons)")
-            elif jug2 == jug_capacity_2:
-                jug2 = 0
-                steps.append(f"Empty jug 2 ({jug_capacity_2} Gallons)")
-            else:
-                transfer = min(jug1, jug_capacity_2 - jug2)
-                jug1 -= transfer
-                jug2 += transfer
-                steps.append(f"Pour {transfer} Gallons from jug 1 to jug 2")
-
+        steps_start_1 = water_jug_solver_from(
+            jug_capacity_1=jug_capacity_1,
+            jug_capacity_2=jug_capacity_2,
+            target=target,
+            start_jug=0,
+        )
+        steps_start_2 = water_jug_solver_from(
+            jug_capacity_1=jug_capacity_1,
+            jug_capacity_2=jug_capacity_2,
+            target=target,
+            start_jug=1,
+        )
+        steps = (
+            steps_start_1
+            if len(steps_start_1) < len(steps_start_2)
+            else steps_start_2
+        )
     return steps
